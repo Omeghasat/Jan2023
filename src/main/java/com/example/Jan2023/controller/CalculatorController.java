4f84985be0cfcc42d5416200323d2e57eb32bf5a
megha satish <meghasatish22@gmail.com>
2023-01-03 18:02:37 -0800

    my 2 commit

---
+++ src/main/java/com/example/Jan2023/controller/CalculatorController.java
@@ -0,0 +1,32 @@
+package com.example.Jan2023.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculate")
+public class CalculatorController {
+    @GetMapping("/add/{first}/{second}")
+    public double add(@PathVariable("first") double firstNumber,
+                      @PathVariable("second") double secondNumber) {
+        return firstNumber + secondNumber;
+    }
+    @GetMapping("/sub/{first}/{second}")
+    public double sub(@PathVariable("first") double firstNumber,
+                      @PathVariable("second") double secondNumber){
+        return firstNumber - secondNumber;
+    }
+
+    @GetMapping("/multiply/{first}/{second}")
+    public double multiply ( @PathVariable("first") double firstNumber,
+                             @PathVariable("second") double secondNumber){
+        return firstNumber * secondNumber;
+    }
+    @GetMapping("/divide/{first}/{second}")
+    public double divide ( @PathVariable("first") double firstNumber,
+                           @PathVariable("second") double secondNumber){
+        return firstNumber / secondNumber;
+    }
+}
