c72edabbe9cd6f7d1d1909820999a35f146cbca6
megha satish <meghasatish22@gmail.com>
2023-01-06 14:45:39 -0800

    my commit

---
+++ src/main/java/com/example/Jan2023/model/Customerinfo.java
@@ -0,0 +1,57 @@
+package com.example.Jan2023.model;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
+import org.springframework.data.annotation.Id;
+
+@EntityScan
+
+public class Customerinfo {
+    @Id
+    private int customerid;
+    private String customername;
+    private String address;
+    private String city;
+    private int postalcode;
+
+    public int getCustomerid() {
+
+        return customerid;
+    }
+
+    public void setCustomerid(int customerid) {
+        this.customerid = customerid;
+    }
+
+    public String getCustomername() {
+        return customername;
+    }
+
+    public void setCustomername(String customername) {
+        this.customername = customername;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getPostalcode() {
+        return postalcode;
+    }
+
+    public void setPostalcode(int postalcode) {
+        this.postalcode = postalcode;
+    }
+}
